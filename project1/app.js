0a4100aa723fd1bdc8a13aed918e1472e39476f3
toomie <90758596+oomie@users.noreply.github.com>
2022-09-25 18:01:35 +0400

    Update app.js

--- project1/app.js
+++ project1/app.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', () => {
     console.log('page is loaded');
 
+    //audio effect:
+    let ding = document.getElementById("ding");
+
     let recreational = document.getElementById('recreational');
     recreational.addEventListener('click', () => {
         fetch("http://www.boredapi.com/api/activity?type=recreational")
@@ -9,7 +12,8 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
-    })
+            ding.play();
+        })
 
     let education = document.getElementById('education');
     education.addEventListener('click', () => {
@@ -19,7 +23,8 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
-    })
+            ding.play();
+        })
 
     let social = document.getElementById('social');
     social.addEventListener('click', () => {
@@ -29,7 +34,8 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
-    })
+            ding.play();
+        })
 
     let diy = document.getElementById('diy');
     diy.addEventListener('click', () => {
@@ -39,7 +45,8 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
-    })
+            ding.play();
+        })
     
     let charity = document.getElementById('charity');
     charity.addEventListener('click', () => {
@@ -49,7 +56,8 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
-    })
+            ding.play();
+        })
     
     let cooking = document.getElementById('cooking');
     cooking.addEventListener('click', () => {
@@ -59,7 +67,8 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
-    })
+            ding.play();
+        })
      
     let relaxation = document.getElementById('relaxation');
     relaxation.addEventListener('click', () => {
@@ -69,7 +78,8 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
-    })
+            ding.play();
+        })
 
     let music = document.getElementById('music');
     music.addEventListener('click', () => {
@@ -79,7 +89,8 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
-    })
+            ding.play();
+        })
 
     let busywork = document.getElementById('busywork');
     busywork.addEventListener('click', () => {
@@ -89,14 +100,12 @@ window.addEventListener('load', () => {
                 let nameelement = this.document.getElementById("activity-name");
                 nameelement.innerHTML = data.activity;
             })
+            ding.play();
     })
 
     let randomtextappear = document.getElementById("randomtextappear");
     randomtextappear.style.display = "none";
     //the text doesnt appear until you click the button
-
-    //audio effect:
-    let ding = document.getElementById("ding");
 
     let random = document.getElementById('random');
     random.addEventListener('click', () => {
@@ -114,3 +123,5 @@ window.addEventListener('load', () => {
     
 
 }) //end
+
+    // link to api https://www.boredapi.com/
